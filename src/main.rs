4f62ae30bd84539aaac50f25265aabb0ee6a6d56
Artjom Löbsack <ceigh@pm.me>
2020-05-18 01:20:35 +0500

    :sparkles: Finish work

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ extern crate rand;
 
 
 fn main() {
-    let (width, height) = (20, 20);
-    draw_rule(width, height);
+    let (width, height) = (50, 20);
+    let rule = [false, true, true, false, true, true, true, false];
+    draw_rule(width, height, rule);
 }
 
 
-fn draw_rule(width: u8, height: u8) {
+fn draw_rule(width: u8, height: u8, rule: [bool; 8]) {
+    let mut line = get_random_line(width);
     for _ in 0..height {
-        let random_line = get_random_line(width);
-        write_line(random_line);
+        write_line(line.clone());
+        line = get_next_line(line, rule);
     }
 }
 
@@ -33,4 +35,28 @@ fn mask_line(line: Vec<bool>) -> Vec<char> {
     line.into_iter().map(|i| {
         if i { mask[1] } else { mask[0] }
     }).collect()
+}
+
+fn get_next_line(previous_line: Vec<bool>, rule: [bool; 8]) -> Vec<bool> {
+    previous_line.clone().into_iter().enumerate().map(|(i, _)| {
+        rule[get_position(previous_line.clone(), i)]
+    }).collect()
+}
+
+fn get_position(line: Vec<bool>, i: usize) -> usize {
+    let last = line.len() - 1;
+
+    let (x, y, z) =
+        if      i == 0    { (line[last],     line[0],    line[1])     }
+        else if i == last { (line[last - 1], line[last], line[0])     }
+        else              { (line[i - 1],    line[i],    line[i + 1]) };
+
+    if       x &&  y &&  z { 0 } // 111
+    else if  x &&  y && !z { 1 } // 110
+    else if  x && !y &&  z { 2 } // 101
+    else if  x && !y && !z { 3 } // 100
+    else if !x &&  y && !z { 4 } // 011
+    else if !x &&  y && !z { 5 } // 010
+    else if !x && !y && !z { 6 } // 001
+    else                   { 7 } // 000
 }
